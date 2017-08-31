b245f008f81d10d853eb88e27d231d5b54b1f12c
Ari Höysniemi <ari.hoysniemi@gmail.com>
2017-08-31 20:30:52 +0300

    Template for a new game logic.

---
+++ src/game.js
@@ -0,0 +1,61 @@
+/**
+ * Here we initialize and run the game logic and therefore the game itself.
+ */
+const Level = require('./src/class.level');
+const Input = require('./src/class.input');
+
+ const Game = {};
+
+ /**
+  * Initializes the game.
+  * @param {object} payload: The initial game config (see EOF).
+  */
+ Game.fncInit = (payload) => {
+   Game.time = 0;
+   Game.level = new Level(payload.level);
+   Game.camera = new Camera(Game.level.camera);
+   Game.input = new Input();
+   Game.tickLength = payload.tickLength;
+   Game.lastTick = performance.now();
+   Game.fncMain(Game.lastTick);
+ };
+
+ /**
+  * The main game loop.
+  * @param {number} raFrame: The current animation frame.
+  */
+ Game.fncMain = (raFrame) => {
+   Game.stopFncMain = window.requestAnimationFrame(() => Game.fncMain(performance.now()));
+   Game.fncQueue(
+    raFrame > Game.lastTick + Game.tickLength
+    ? Math.floor((raFrame - Game.lastTick) / Game.tickLength) : 0
+   );
+ };
+
+/**
+* Queues a game logic update.
+* @param {number} tickC: The amount of ticks to be processed.
+*/
+Game.fncQueue = (tickC) => {
+  for (let i = 0; i < tickC; i++) {
+    Game.lastTick = Game.lastTick + Game.tickLength;
+    Game.fncUpdate(Game.lastTick);
+  }
+};
+ 
+ /**
+  * Updates the game logic.
+  * Controlled by the main game loop.
+  * @param {number} lastTick: Tick to be updated.
+  */
+ Game.fncUpdate = (lastTick) => {
+   
+ };
+
+ /**
+  * Input the initial configuration for the game.
+  */
+ Game.fncInit({
+   level: 'lvl_cube',
+   tickLength: 50,
+ });
