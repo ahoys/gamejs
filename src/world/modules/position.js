894c6f4a8cc945089adeb81a7c955e18df816ff8
Ari Höysniemi <ari.hoysniemi@gmail.com>
2017-09-09 23:49:13 +0300

    More flexible module format for position.

--- src/world/modules/position.js
+++ src/world/modules/position.js
@@ -1,25 +1,47 @@
-module.exports = {
+/**
+ * Position
+ * Properties for following object position relative to the world.
+ */
+const position = {
   x: 0,
   y: 0,
   z: 0,
-  setX: function(v) {
-    this.x = Number(v);
-  },
-  setY: function(v) {
-    this.y = Number(v);
-  },
-  setZ: function(v) {
-    this.z = Number(v);
-  },
-  setPosition: function(pos = []) {
-    if (pos[0] !== undefined) {
-      this.x = Number(pos[0]);
-    }
-    if (pos[1] !== undefined) {
-      this.y = Number(pos[1]);
-    }
-    if (pos[2] !== undefined) {
-      this.z = Number(pos[2]);
-    }
-  },
-}
+};
+
+position.setX = function(v) {
+  this.x = Number(v);
+};
+
+position.setY = function(v) {
+  this.y = Number(v);
+};
+
+position.setZ = function(v) {
+  this.z = Number(v);
+};
+
+/**
+ * Set all values at once.
+ * @param {array} pos [x, y, z].
+ */
+position.setPosition = function(pos) {
+  if (pos[0] !== undefined) {
+    this.x = Number(pos[0]);
+  }
+  if (pos[1] !== undefined) {
+    this.y = Number(pos[1]);
+  }
+  if (pos[2] !== undefined) {
+    this.z = Number(pos[2]);
+  }
+};
+
+/**
+ * Return all values at once.
+ * @returns {array} [x, y, z].
+ */
+position.getPosition = function() {
+  return [this.x, this.y, this.z];
+};
+
+module.exports = position;
